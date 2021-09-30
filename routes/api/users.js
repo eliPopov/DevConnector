de44b322495a4b656ec267c729be30a5b2ca7119
Eli Popov <eli.popov96@gmail.com>
2021-09-30 15:58:49 +0300

    added user authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 
-const User = require('../../models/User');
+let User = require('../../models/User');
 
 //@route   Post api/users
 //@desc    Register user
@@ -57,9 +59,21 @@ router.post(
       await user.save();
 
       //Return jsonwebtoken
-
-      res.send('User registered');
-    } catch {
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
     }
